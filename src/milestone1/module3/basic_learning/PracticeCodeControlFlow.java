b047fc625f30f2e5c9c9b2177a235b321baf1708
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-07 23:57:09 +0600

    Add practice on control flow

---
+++ src/milestone1/module3/basic_learning/PracticeCodeControlFlow.java
@@ -0,0 +1,28 @@
+package milestone1.module3.basic_learning;
+
+public class PracticeCodeControlFlow {
+    public static void main(String[] params){
+//        int age = 8;
+//        if(age < 10){
+//            System.out.println("Child");
+//            System.out.printf("Age is: %d\n", age);
+//        }
+//        else {
+//            System.out.println("Not child");
+
+
+//        int i = 3;
+//        while (i++ < 10) {
+//            System.out.println(i--);   // Infinite loop
+//        }
+
+
+
+        int i =1;
+        while(i++ < 10){
+            System.out.println(i++);
+        }
+
+
+    }
+}
