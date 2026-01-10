ce93d3af28569d6d9c0de8d88260808930d9d2b8
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-11 01:59:54 +0600

    Professional Input Loop with  Scanner class

--- src/milestone1/module4/module4part2/ScannerClassLearning.java
+++ src/milestone1/module4/module4part2/ScannerClassLearning.java
@@ -4,28 +4,47 @@ import java.util.Scanner;
 
 
 public class ScannerClassLearning {
-        public static void main(String args[]) {
+    public static void main(String args[]) {
 
-        // ______________________________________________________________________________
-        // To understand hasNext(), just do the following code
+    // ______________________________________________________________________________
+    // To understand hasNext(), just do the following code
 //        Scanner input = new Scanner(System.in);
 //
 //        if (input.hasNext()){
 //            System.out.println("User typed something!");
 //        }
-        // ______________________________________________________________________________
+    // ______________________________________________________________________________
 
 
-        // ______________________________________________________________________________
-        // To understand hasNextX(), just do the following code
+    // ______________________________________________________________________________
+    // To understand hasNextX(), just do the following code
+//        Scanner input = new Scanner(System.in);
+//
+//        if (input.hasNextDouble()){
+//            double x = input.nextDouble();
+//            System.out.println("Printing the double: " + x);
+//        } else {
+//            System.out.println("Invalid Input");
+//            input.next();
+//        }
+
+
+    // ___________________________________________________________________________
+    // Correct professional input loop
         Scanner input = new Scanner(System.in);
 
-        if (input.hasNextDouble()){
-            double x = input.nextDouble();
-            System.out.println("Printing the double: " + x);
-        } else {
-            System.out.println("Invalid Input");
-            input.next();
+        while (true) {
+            System.out.println("You Entered into the loop");
+
+            if (input.hasNextDouble()) {
+                double x = input.nextDouble();
+
+                System.out.println("You entered: " + x);
+                break;
+            } else {
+                System.out.println("Invalid Input");
+                input.next();   // without it, the loop becomes infinite
+            }
         }
 
     }
