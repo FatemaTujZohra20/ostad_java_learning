9ec399fac21e9578e2f6c780fcd5b5e6837518b3
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-11 01:42:41 +0600

    Add hasNext() in Scanner class

---
+++ src/milestone1/module4/module4part2/ScannerClassLearning.java
@@ -0,0 +1,21 @@
+package milestone1.module4.module4part2;
+
+import java.util.Scanner;
+
+
+public class ScannerClassLearning {
+        public static void main(String args[]) {
+
+        // ______________________________________________________________________________
+        // To understand hasNext(), just do the following code
+        Scanner input = new Scanner(System.in);
+
+        if (input.hasNext()){
+            System.out.println("User typed something!");
+        }
+        // ______________________________________________________________________________
+
+    }
+}
+
+
