c9d8cbca08178e988a890faeb1c4343059c66183
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-10 22:25:21 +0600

    Add while loop usage

--- src/milestone1/module3/module3project/SimpleGradingSystem.java
+++ src/milestone1/module3/module3project/SimpleGradingSystem.java
@@ -5,48 +5,53 @@ import java.util.Scanner;
 public class SimpleGradingSystem {
     public static void main(String[] args){
         Scanner input = new Scanner((System.in));
+        while(true) {
 
-        double marks;
-        String grade;
-        String verdict;
+            double marks;
+            String grade;
+            String verdict;
 
-        System.out.println("Enter your marks below: ");
-        marks = input.nextDouble();
+            System.out.println();
+            System.out.println("----------------------------------------");
+            System.out.println("Enter any negative number to exit.");
+            System.out.println("Enter your marks below: ");
+            marks = input.nextDouble();
 
-        if(marks > 100) {
-            System.out.println("Marks cannot be greater than 100");
-        }
-        if(marks < 0){
-            System.out.println("Marks cannot be less than 0");
-        }
+            if (marks > 100) {
+                System.out.println("Marks cannot be greater than 100");
+            }
+            if (marks < 0) {
+                break;
+            }
 
-        if(marks <= 100 && marks >= 90){
-            grade = "A";
-            verdict = "Excellent";
-            System.out.println("Your grade is " + grade + " and your verdict is " + verdict);
-        } else if (marks <= 89 && marks >= 80){
-            grade = "B";
-            verdict = "Good";
-            System.out.println("Your grade is " + grade + " and your verdict is " + verdict);
-        } else if (marks <=79 && marks >= 70){
-            grade = "C";
-            verdict = "Average";
-            System.out.println("Your grade is " + grade + " and your verdict is " + verdict);
-        } else if (marks <= 69 && marks >=60){
-            grade = "D";
-            verdict = "Below Average";
-            System.out.println("Your grade is " + grade + " and your verdict is " + verdict);
-        } else if (marks < 60 && marks >= 0){
-            grade = "F";
-            verdict = "Fail";
-            System.out.println("Your grade is " + grade + " and your verdict is " + verdict);
+            if (marks <= 100 && marks >= 90) {
+                grade = "A";
+                verdict = "Excellent";
+                System.out.println("Your grade is " + grade + " and your verdict is " + verdict);
+            } else if (marks <= 89 && marks >= 80) {
+                grade = "B";
+                verdict = "Good";
+                System.out.println("Your grade is " + grade + " and your verdict is " + verdict);
+            } else if (marks <= 79 && marks >= 70) {
+                grade = "C";
+                verdict = "Average";
+                System.out.println("Your grade is " + grade + " and your verdict is " + verdict);
+            } else if (marks <= 69 && marks >= 60) {
+                grade = "D";
+                verdict = "Below Average";
+                System.out.println("Your grade is " + grade + " and your verdict is " + verdict);
+            } else if (marks < 60 && marks >= 0) {
+                grade = "F";
+                verdict = "Fail";
+                System.out.println("Your grade is " + grade + " and your verdict is " + verdict);
 
-            if(marks <= 59 && marks >= 50){
-                System.out.println("You almost got it. Try a little harder next time.");
-            } else if (marks <= 49 && marks >= 40){
-                System.out.println("Study more seriously than you do.");
-            } else{
-                System.out.println("Meet me at my office tomorrow. We need to have a serious discussion.");
+                if (marks <= 59 && marks >= 50) {
+                    System.out.println("You almost got it. Try a little harder next time.");
+                } else if (marks <= 49 && marks >= 40) {
+                    System.out.println("Study more seriously than you do.");
+                } else {
+                    System.out.println("Meet me at my office tomorrow. We need to have a serious discussion.");
+                }
             }
         }
     }
