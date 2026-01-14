3b5ab73dedaeab65e4cb0d75bdea6abb566fde37
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-15 02:10:39 +0600

    feat: add Pet base class

---
+++ src/milestone1/module6/module6Assignment/Pet.java
@@ -0,0 +1,34 @@
+package milestone1.module6.module6Assignment;
+
+public class Pet {
+    private String name;
+    private int age;
+    private String breed;
+
+    public Pet(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+
+    // Getters and Setters
+    public String getName() { return name; }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() { return age; }
+    public void setAge(int age) {
+        this. age = age;
+    }
+
+    public String getBreed() { return breed; }
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void makeSound() {
+        System.out.println("Pet makes a sound");
+    }
+}
