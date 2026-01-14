7f6abac278cff84dafd299811cd1a197da292115
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-15 00:47:59 +0600

    feat: implement Dog class following Animal interface

---
+++ src/milestone1/module6/module6part2/Interface/Dog.java
@@ -0,0 +1,8 @@
+package milestone1.module6.module6part2.Interface;
+
+// 2. Implementation: Dog
+public class Dog implements Animal {
+    public void makeSound() {
+        System.out.println("Dog says: Woof Woof!");
+    }
+}
