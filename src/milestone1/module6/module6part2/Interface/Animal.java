c531a99a0cf3f326bb761f2204ad6d107927fb97
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-15 00:47:21 +0600

    feat: define Animal interface contract

---
+++ src/milestone1/module6/module6part2/Interface/Animal.java
@@ -0,0 +1,6 @@
+package milestone1.module6.module6part2.Interface;
+
+// 1. The Interface (The "Rulebook")
+public interface Animal {
+    void makeSound(); // No body need, just the name of the action
+}
